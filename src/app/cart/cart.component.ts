fa1677587ef70d73613185c30e93b93f7c705af4
aimphaka <71070260+aimphaka@users.noreply.github.com>
2021-12-25 22:25:27 +0700

    using 'FormBuilder' for gather data

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 import { CartService } from '../cart.service';
 
 
@@ -12,11 +14,30 @@ export class CartComponent implements OnInit {
   //  call method 'getAllItems()' from CartService (using instance injection), return 'all products' that stored in an arrays
   products = this.cartService.getAllItems();
   
+  //  create "form checkout" for gather 'user's information'
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+  
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
+  }
+
+  //  method for process data from user'form checkout
+  onSubmit(): void {
+    // console.warning with data from 'checkfoutForm'
+    console.warn(`your order has been submitted`, this.checkoutForm.value);
+    
+    //  clear all products in cart
+    this.products = this.cartService.clearCart();
+
+
+    this.checkoutForm.reset(); // reset form
   }
 
 }
