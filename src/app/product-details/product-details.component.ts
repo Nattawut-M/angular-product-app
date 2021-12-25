0508920e40b7e7d1fe0bded6691b2642cccd2ca3
aimphaka <71070260+aimphaka@users.noreply.github.com>
2021-12-25 18:42:57 +0700

    learn 'adding navigation', show details of each product

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
+import { products, Product } from '../products';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product : Product | undefined;
+  
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // get 'product id' from route parameters (path variables you define in the route)
+    const routeParams = this.route.snapshot.paramMap;
+    const productId = Number(routeParams.get('productId'));
+
+    //  find the product that in arrays (id of product == id of product from route)  
+    this.product = products.find( product => product.id === productId);
+
   }
 
 }
